cc66852183bb2aaecb32dafc39fb2e4b75edc95a
kiviw <100179542+kiviw@users.noreply.github.com>
2023-11-21 14:12:07 +0300

    Update monero-subaddress-woocommerce.php

--- monero-subaddress-woocommerce.php
+++ monero-subaddress-woocommerce.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Monero Subaddress Generator
- * Description: Generates a Monero subaddress using RPC and displays it on the WooCommerce checkout page.
+ * Description: Generates a Monero subaddress using RPC and displays it on the WooCommerce order received page.
  * Version: 1.2
  * Author: Your Name
  */
@@ -13,36 +13,10 @@ function monero_subaddress_ajax_enqueue() {
     wp_localize_script('monero-subaddress-ajax', 'ajax_object', array('ajax_url' => admin_url('admin-ajax.php')));
 }
 
-// Generate Monero subaddress when user proceeds to checkout
-add_action('woocommerce_before_checkout_form', 'generate_monero_subaddress_on_checkout');
+// Generate Monero subaddress when user places an order
+add_action('woocommerce_thankyou', 'generate_monero_subaddress_on_order_received', 10, 1);
 
-function generate_monero_subaddress_on_checkout() {
-    ?>
-    <div id="monero-subaddress-container">
-        <p id="monero-subaddress-result">Click the button to generate Monero subaddress.</p>
-        <button id="generate-monero-subaddress">Generate Subaddress</button>
-    </div>
-
-    <script>
-        jQuery(document).ready(function ($) {
-            $('#generate-monero-subaddress').on('click', function () {
-                $.ajax({
-                    type: 'POST',
-                    url: ajax_object.ajax_url,
-                    data: { action: 'generate_monero_subaddress_checkout' },
-                    success: function (response) {
-                        $('#monero-subaddress-result').html('Generated Monero Subaddress: ' + response);
-                    }
-                });
-            });
-        });
-    </script>
-    <?php
-}
-
-// AJAX action for subaddress generation on checkout
-add_action('wp_ajax_generate_monero_subaddress_checkout', 'generate_monero_subaddress_callback_checkout');
-function generate_monero_subaddress_callback_checkout() {
+function generate_monero_subaddress_on_order_received($order_id) {
     $rpc_url = 'http://127.0.0.1:18080/json_rpc'; // Replace with your Monero RPC URL
 
     $request_body = json_encode([
@@ -55,7 +29,7 @@ function generate_monero_subaddress_callback_checkout() {
     ]);
 
     // Log the request for debugging
-    error_log('Monero RPC Request for Checkout: ' . $request_body);
+    error_log('Monero RPC Request for Order ' . $order_id . ': ' . $request_body);
 
     $response = wp_remote_post($rpc_url, [
         'body' => $request_body,
@@ -68,16 +42,31 @@ function generate_monero_subaddress_callback_checkout() {
         $body = wp_remote_retrieve_body($response);
 
         // Log the response for debugging
-        error_log('Monero RPC Response for Checkout: ' . $body);
+        error_log('Monero RPC Response for Order ' . $order_id . ': ' . $body);
 
         $result = json_decode($body, true);
 
         if (isset($result['result']['address'])) {
-            echo $result['result']['address'];
+            // Update the order with the generated subaddress
+            update_post_meta($order_id, '_monero_subaddress', $result['result']['address']);
         } else {
-            echo 'Error generating Monero subaddress';
+            // Log an error message if the response format is unexpected
+            error_log('Monero RPC Error: Unexpected response format for Order ' . $order_id);
+
+            // Log the unexpected response for further investigation
+            error_log('Monero RPC Unexpected Response: ' . $body);
         }
     }
-
-    wp_die();
 }
+
+// Display the subaddress on the WooCommerce order received page
+add_action('woocommerce_thankyou', 'display_monero_subaddress_on_order_received', 20);
+
+function display_monero_subaddress_on_order_received($order_id) {
+    $subaddress = get_post_meta($order_id, '_monero_subaddress', true);
+
+    if (!empty($subaddress)) {
+        echo '<p id="monero-subaddress-container"><strong>Monero Subaddress:</strong> ' . esc_html($subaddress) . '</p>';
+    }
+}
+?>
