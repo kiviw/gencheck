180e577a8eaad81054c9e8cc636bd8b4fa83ecf0
kiviw <100179542+kiviw@users.noreply.github.com>
2023-11-21 13:13:55 +0300

    Update monero-subaddress-woocommerce.php

--- monero-subaddress-woocommerce.php
+++ monero-subaddress-woocommerce.php
@@ -2,17 +2,47 @@
 /**
  * Plugin Name: Monero Subaddress Generator
  * Description: Generates a Monero subaddress using RPC and displays it on the WooCommerce checkout page.
- * Version: 1.1
+ * Version: 1.2
  * Author: Your Name
  */
 
-function generate_monero_subaddress_on_order($order_id) {
-    // Debugging: Before retrieving order ID
-    error_log('Debugging: Before retrieving order ID');
-    $order_id = wc_get_checkout_order_received_id();
-    // Debugging: After retrieving order ID
-    error_log('Debugging: After retrieving order ID');
+// Enqueue JavaScript for AJAX functionality
+add_action('wp_enqueue_scripts', 'monero_subaddress_ajax_enqueue');
+function monero_subaddress_ajax_enqueue() {
+    wp_enqueue_script('monero-subaddress-ajax', plugin_dir_url(__FILE__) . 'monero-subaddress-ajax.js', array('jquery'), '1.0', true);
+    wp_localize_script('monero-subaddress-ajax', 'ajax_object', array('ajax_url' => admin_url('admin-ajax.php')));
+}
 
+// Generate Monero subaddress when user proceeds to checkout
+add_action('woocommerce_before_checkout_form', 'generate_monero_subaddress_on_checkout');
+
+function generate_monero_subaddress_on_checkout() {
+    ?>
+    <div id="monero-subaddress-container">
+        <p id="monero-subaddress-result">Click the button to generate Monero subaddress.</p>
+        <button id="generate-monero-subaddress">Generate Subaddress</button>
+    </div>
+
+    <script>
+        jQuery(document).ready(function ($) {
+            $('#generate-monero-subaddress').on('click', function () {
+                $.ajax({
+                    type: 'POST',
+                    url: ajax_object.ajax_url,
+                    data: { action: 'generate_monero_subaddress_checkout' },
+                    success: function (response) {
+                        $('#monero-subaddress-result').html('Generated Monero Subaddress: ' + response);
+                    }
+                });
+            });
+        });
+    </script>
+    <?php
+}
+
+// AJAX action for subaddress generation on checkout
+add_action('wp_ajax_generate_monero_subaddress_checkout', 'generate_monero_subaddress_callback_checkout');
+function generate_monero_subaddress_callback_checkout() {
     $rpc_url = 'http://127.0.0.1:18080/json_rpc'; // Replace with your Monero RPC URL
 
     $request_body = json_encode([
@@ -20,12 +50,12 @@ function generate_monero_subaddress_on_order($order_id) {
         'id' => '0',
         'method' => 'create_address',
         'params' => [
-            'count' => 1, // Adjust the count as needed
+            'count' => 1,
         ],
     ]);
 
     // Log the request for debugging
-    error_log('Monero RPC Request for Order ' . $order_id . ': ' . $request_body);
+    error_log('Monero RPC Request for Checkout: ' . $request_body);
 
     $response = wp_remote_post($rpc_url, [
         'body' => $request_body,
@@ -34,44 +64,20 @@ function generate_monero_subaddress_on_order($order_id) {
         ],
     ]);
 
-    if (is_wp_error($response)) {
-        // Log the error for debugging
-        error_log('Error generating Monero subaddress for Order ' . $order_id . ': ' . $response->get_error_message());
-    } else {
+    if (!is_wp_error($response)) {
         $body = wp_remote_retrieve_body($response);
 
         // Log the response for debugging
-        error_log('Monero RPC Response for Order ' . $order_id . ': ' . $body);
+        error_log('Monero RPC Response for Checkout: ' . $body);
 
         $result = json_decode($body, true);
 
         if (isset($result['result']['address'])) {
-            // Update the order with the generated subaddress
-            update_post_meta($order_id, '_monero_subaddress', $result['result']['address']);
+            echo $result['result']['address'];
         } else {
-            // Log an error message if the response format is unexpected
-            error_log('Monero RPC Error: Unexpected response format for Order ' . $order_id);
-
-            // Log the unexpected response for further investigation
-            error_log('Monero RPC Unexpected Response: ' . $body);
+            echo 'Error generating Monero subaddress';
         }
     }
-}
 
-add_action('woocommerce_new_order', 'generate_monero_subaddress_on_order', 10, 1);
-
-// Display the subaddress on the WooCommerce checkout page
-add_action('woocommerce_before_checkout_form', 'display_monero_subaddress_on_checkout');
-
-function display_monero_subaddress_on_checkout() {
-    // Debugging: Before retrieving subaddress
-    error_log('Debugging: Before retrieving subaddress');
-    $order_id = wc_get_checkout_order_received_id();
-    $subaddress = get_post_meta($order_id, '_monero_subaddress', true);
-    // Debugging: After retrieving subaddress
-    error_log('Debugging: After retrieving subaddress');
-
-    if (!empty($subaddress)) {
-        echo '<p id="monero-subaddress-container"><strong>Monero Subaddress:</strong> ' . esc_html($subaddress) . '</p>';
-    }
+    wp_die();
 }
